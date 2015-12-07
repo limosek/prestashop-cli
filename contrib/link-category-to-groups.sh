d2a0b690a8e7ee230f2ebca1c992dc748d1efff0
Lukas Macura <macura@opf.slu.cz>
2015-12-07 13:52:41 +0100

    Added contrib script to manage category and group associations

---
+++ contrib/link-category-to-groups.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#######################################################################################################################################
+## This script is used as practical guide to prestashop-cli 
+## This example will get all categories matched by "$1" and all groups matched by "$2" and link all groups to this categories.
+## Because there is no API call for managing this associations, result of this script is SQL script which has to be imported afterwards.
+########################################################################################################################################
+
+categoryregexp="$1"
+groupregexp="$2"
+
+if [ -z "$2" ]; then
+  echo "$0 categoryregexp grouprexexp"
+  echo "Use .* to match all of them."
+  exit 1
+fi 
+
+groups=$(pslist groups "name~$groupregexp")
+categories=$(pslist categories "name~$categoryregexp")
+echo "Groups: $groups" >&2
+echo "Categories: $categories" >&2
+
+echo "BEGIN"
+for c in $categories; do
+  for g in $groups; do
+	echo "DELETE FROM ps_category_group WHERE id_category=$c AND id_group=$g;"
+	echo "INSERT IGNORE INTO ps_category_group (id_category,id_group) VALUES ($c,$g);"
+  done
+done
+echo "COMMIT"
+
