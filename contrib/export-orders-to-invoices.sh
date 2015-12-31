bcd872b3f9b1ea8ff91bfb90e4c4b895b33417bb
Lukas Macura <lukas@macura.cz>
2015-12-31 17:57:33 +0100

    Modified export psprice is working

--- contrib/export-orders-to-invoices.sh
+++ contrib/export-orders-to-invoices.sh
@@ -1,27 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 ###########################################################
 ## This script is used as practical guide to prestashop-cli 
 ## It will export orders to import as invoices
+## 
 ## Arguments are filter(s) to match orders
 ###########################################################
 
-orders=$(pslist orders)
-eval $(pslist -Fenvarr orders "$@" invoice_number invoice_date reference)
+if [ -z "$2" ]; then
+  echo $0 "invoice_type payment [filter].."
+  exit 2
+fi
+
+invoice_type="$1"
+shift
+payment="$1"
+shift
+
+csvdata(){
+  local i
+  for i in "$@"; do
+    echo -En '"'$i'";'
+  done
+}
+
+orders=$(pslist orders payment="$payment" "$@")
+eval $(pslist -Fenvarr orders "$@" date_add invoice_number invoice_date reference id_customer id_address_invoice id_address_delivery payment total_products total_shipping total_discounts)
 
 for o in $orders; do
   eval $(pslist -Fenvarr order_details id_order=$o)
 done
 
-
-eval $(pslist -Fenvarr orders "$@" invoice_number invoice_date reference)
-
 for o in $orders; do
-    eval $(psget -Fenv customer $c)
-    cid=$(expr $c + $idshift)
-    address=$(pslist addresses id_customer=$id | head -1)
-    if [ -n "$address" ]; then
-      eval $(psget -Fenv address $address)
-      echo "$cid;$lastname;$firstname;$address1;$city;$postcode;$phone;$phone_mobile;$email"
-    fi
+    eval $(psget -Fenv customer ${orders[$o,id_customer]})
+    cid=$id_customer
+    daddress=${orders[$o,id_address_delivery]}
+    iaddress=${orders[$o,id_address_invoice]}
+    eval $(psget -Fenv address $iaddress)
+    eval $(psget -Fenv address $daddress)
+    [ "${orders[$o,invoice_number]}" = 0 ] && continue
+    #         1  2                          	3				4				5
+    csvdata "$o" ${orders[$o,invoice_number]} 	"${orders[$o,reference]}" 	"${orders[$o,date_add]}"	"$lastname"
+    #		6		7		8	9		10		11		12
+    csvdata "$firstname" 	"$address1"	"$city"	"$postcode"	"$phone"	"$phone_mobile"	"$email"
+    #		13				14				15 				16			17
+    csvdata "${orders[$o,total_products]}"	${orders[$o,total_shipping]}	${orders[$o,total_discounts]}	"${orders[$o,payment]}"	"$invoice_type"
+    echo
 done
